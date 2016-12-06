fd6320c6f66b61fe0204dd7f1d8a6f329f25a331
Dan Broadbent <dbroadbent85@gmail.com>
2016-12-05 20:55:59 -0700

    Add payment model

--- spec/loans_have_payments_spec.rb
+++ spec/loans_have_payments_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
-describe 'loans have payment' do
+describe 'loans have payments' do
   it 'has one payment' do
     loan = Loan.create(funded_amount: 100.00)
-    payment = loan.payments.create(amount: 10.00)
+    payment = loan.payments.create(loan_id: 1, amount: 10.00)
 
     expect(payment).to eq(loan.payments.first)
   end
